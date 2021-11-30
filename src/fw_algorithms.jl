6c14f0b338b70a8b414a22b50c8eaf386866a4b4
morgankohler <kohler.r.morgan@gmail.com>
2021-11-29 20:15:03 -0500

    start of changes

--- src/fw_algorithms.jl
+++ src/fw_algorithms.jl
@@ -224,6 +224,282 @@ function frank_wolfe(
 end
 
 
+function frank_wolfe_2var(
+    f,
+    grad!,
+    lmo_x,
+    lmo_p,
+    x0,  # this is s0
+    p0;
+    line_search::LineSearchMethod=Adaptive(),
+    L=Inf,
+    gamma0=0,
+    step_lim=20,
+    momentum=nothing,
+    epsilon=1e-7,
+    max_iteration=10000,
+    print_iter=1000,
+    trajectory=false,
+    verbose=false,
+    linesearch_tol=1e-7,
+    emphasis::Emphasis=memory,
+    gradient=nothing,
+    callback=nothing,
+    timeout=Inf,
+    print_callback=print_callback,
+)
+
+    # format string for output of the algorithm
+    format_string = "%6s %13s %14e %14e %14e %14e %14e\n"
+    gamma0_x = 0
+    gamma0_p = 0
+    L_x = Inf
+    L_p = Inf
+    t = 0
+    dual_gap_x = Inf
+    dual_gap_p = Inf
+    primal = Inf
+    v_x = []
+    v_p = []
+    x = x0
+    p = p0
+    tt = regular
+    traj_data = []
+    if trajectory && callback === nothing
+        callback = trajectory_callback(traj_data)
+    end
+    time_start = time_ns()
+
+    if line_search isa Shortstep && !isfinite(L)
+        println("FATAL: Lipschitz constant not set. Prepare to blow up spectacularly.")
+    end
+
+    if line_search isa FixedStep && gamma0 == 0
+        println("FATAL: gamma0 not set. We are not going to move a single bit.")
+    end
+
+    if (!isnothing(momentum) && line_search isa Union{Shortstep,Adaptive,RationalShortstep})
+        println(
+            "WARNING: Momentum-averaged gradients should usually be used with agnostic stepsize rules.",
+        )
+    end
+
+    if verbose
+        println("\nVanilla Frank-Wolfe Algorithm.")
+        numType = eltype(x0)
+        println(
+            "EMPHASIS: $emphasis STEPSIZE: $line_search EPSILON: $epsilon MAXITERATION: $max_iteration TYPE: $numType",
+        )
+        grad_type = typeof(gradient)
+        println("MOMENTUM: $momentum GRADIENTTYPE: $grad_type")
+        if emphasis === memory
+            println("WARNING: In memory emphasis mode iterates are written back into x0!")
+        end
+        headers = ["Type", "Iteration", "Primal", "Dual", "Dual Gap", "Time", "It/sec"]
+        print_callback(headers, format_string, print_header=true)
+    end
+    if emphasis == memory && !isa(x, Union{Array,SparseArrays.AbstractSparseArray})
+        # if integer, convert element type to most appropriate float
+        if eltype(x) <: Integer
+            x = copyto!(similar(x, float(eltype(x))), x)
+        else
+            x = copyto!(similar(x), x)
+        end
+    end
+    if emphasis == memory && !isa(p, Union{Array,SparseArrays.AbstractSparseArray})
+        # if integer, convert element type to most appropriate float
+        if eltype(p) <: Integer
+            p = copyto!(similar(p, float(eltype(p))), p)
+        else
+            p = copyto!(similar(p), p)
+        end
+    end
+    first_iter = true
+    # instanciating container for gradient
+
+    gradient_x = similar(x)
+    gradient_p = similar(x)
+
+    # container for direction
+    d_x = similar(x)
+    d_p = similar(x)
+    gtemp_x = similar(x)
+    gtemp_p = similar(x)
+
+    gtemp = Nothing
+
+    while t <= max_iteration # && dual_gap >= max(epsilon, eps())
+
+        #####################
+        # managing time and Ctrl-C
+        #####################
+        time_at_loop = time_ns()
+        if t == 0
+            time_start = time_at_loop
+        end
+        # time is measured at beginning of loop for consistency throughout all algorithms
+        tot_time = (time_at_loop - time_start) / 1e9
+
+        if timeout < Inf
+            if tot_time ≥ timeout
+                if verbose
+                    @info "Time limit reached"
+                end
+                break
+            end
+        end
+
+        #####################
+
+
+        if momentum === nothing || first_iter
+            grad!(gradient, x, p)
+            gradient_x = gradient[1]
+            gradient_p = gradient[2]
+            if momentum !== nothing
+                gtemp_x .= gradient[1]
+                gtemp_p .= gradient[2]
+            end
+        else
+            grad!(gtemp, x, p)
+            gtemp_x = gtemp[1]
+            gtemp_p = gtemp[2]
+            @emphasis(emphasis, gradient_x = (momentum_x * gradient_x) + (1 - momentum_x) * gtemp_x)
+            @emphasis(emphasis, gradient_p = (momentum_p * gradient_p) + (1 - momentum_p) * gtemp_p)
+        end
+        first_iter = false
+
+        v_x = compute_extreme_point(lmo_x, gradient_x)
+        v_p = compute_extreme_point(lmo_p, gradient_p)
+        # go easy on the memory - only compute if really needed
+        if (
+            (mod(t, print_iter) == 0 && verbose) ||
+            callback !== nothing ||
+            line_search isa Shortstep
+        )
+            primal = f(x, p)
+            dual_gap_x = fast_dot(x, gradient_x) - fast_dot(v_x, gradient_x)
+            dual_gap_p = fast_dot(p, gradient_p) - fast_dot(v_p, gradient_p)
+        end
+
+        @emphasis(emphasis, d_x = x - v_x)
+        @emphasis(emphasis, d_p = p - v_p)
+
+        f_x = (x_local) -> f(x_local, p)
+        f_p = (p_local) -> f(x, p_local)
+        g_x = (grad_local, x_local) -> grad!(grad_local, x_local, p)
+        g_p = (grad_local, p_local) -> grad!(grad_local, x, p_local)
+
+        gamma_x, L_x = line_search_wrapper(
+            line_search,
+            t,
+            f_x,
+            g_x,
+            x,
+            d_x,
+            momentum_x === nothing ? gradient_x : gtemp_x, # use appropriate storage
+            dual_gap_x,
+            L_x,
+            gamma0_x,
+            linesearch_tol,
+            step_lim,
+            one(eltype(x)),
+        )
+        gamma_p, L_p = line_search_wrapper(
+            line_search,
+            t,
+            f_p,
+            g_p,
+            p,
+            d_p,
+            momentum_p === nothing ? gradient_p : gtemp_p, # use appropriate storage
+            dual_gap_p,
+            L_p,
+            gamma0_p,
+            linesearch_tol,
+            step_lim,
+            one(eltype(p)),
+        )
+
+        if callback !== nothing
+            state_x = (
+                t=t,
+                primal=primal,
+                dual=primal - dual_gap_x,
+                dual_gap=dual_gap_p,
+                time=tot_time,
+                x=x,
+                v=v_x,
+                gamma=gamma_x,
+            )
+            callback(state)
+
+            state = (
+                t=t,
+                primal=primal,
+                dual=primal - dual_gap_p,
+                dual_gap=dual_gap_p,
+                time=tot_time,
+                x=p,
+                v=v_p,
+                gamma=gamma_p,
+            )
+            callback(state)
+
+        end
+
+        @emphasis(emphasis, x = x - gamma_x * d_x)
+        @emphasis(emphasis, p = p - gamma_p * d_p)
+
+        if (mod(t, print_iter) == 0 && verbose)
+            tt = regular
+            if t == 0
+                tt = initial
+            end
+
+            rep = (
+                st[Symbol(tt)],
+                string(t),
+                Float64(primal),
+                Float64(primal - dual_gap_p),
+                Float64(dual_gap_p),
+                tot_time,
+                t / tot_time,
+            )
+            print_callback(rep, format_string)
+
+            flush(stdout)
+        end
+        t = t + 1
+    end
+    # recompute everything once for final verification / do not record to trajectory though for now!
+    # this is important as some variants do not recompute f(x) and the dual_gap regularly but only when reporting
+    # hence the final computation.
+
+#     grad!(gradient, x)
+#     v = compute_extreme_point(lmo, gradient)
+#     primal = f(x)
+#     dual_gap = fast_dot(x, gradient) - fast_dot(v, gradient)
+#     if verbose
+#         tt = last
+#         tot_time = (time_ns() - time_start) / 1.0e9
+#         rep = (
+#             st[Symbol(tt)],
+#             string(t - 1),
+#             Float64(primal),
+#             Float64(primal - dual_gap),
+#             Float64(dual_gap),
+#             tot_time,
+#             t / tot_time,
+#         )
+#         print_callback(rep, format_string)
+#         print_callback(nothing, format_string, print_footer=true)
+#         flush(stdout)
+#     end
+    return x, v_x, p, v_p, primal, dual_gap_x, dual_gap_p, traj_data
+end
+
+
 """
     lazified_conditional_gradient
 
